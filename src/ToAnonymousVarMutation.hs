6027aea94cb5dbf65fafec76d46ecee193800131
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-06-01 14:52:56 +0200

    implement rudimentary version of "to-anonymous-variables" (#3)
    * implement rudimentary version of "to-anonymous-variables"
    
    * consider every variable only once
    
    * refactoring
    
    * QuickFix escaping backslash
    
    * also consider variables occurring (only) in right-hand sides

---
+++ src/ToAnonymousVarMutation.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+module ToAnonymousVarMutation (toAnonVarMutation) where
+
+import Language.Prolog
+
+import DropClauseMutation (Mode(..))
+
+import Control.Applicative
+import Data.List
+import Data.List.Extra
+import Data.Maybe
+
+type Error = String
+type ProgramText = String
+
+toAnonVarMutation :: Mode -> ProgramText -> Either Error [ProgramText]
+toAnonVarMutation Summary s =
+  case consultString s of
+    Left e -> Left $ show e
+    Right cs -> Right $ map (concatMap ((++ ".\n") . replace "\\\\" "\\" . init . tail . show)) (tail $ mapM mutate cs)
+      where
+        mutate (Clause lhs rhs) = do
+          anonVars <- anySubsetOf (nub (theVarsIn lhs ++ concatMap theVarsIn rhs))
+          let subst = map (,(Var $ Wildcard $ Nothing)) anonVars
+          return (Clause (apply subst lhs) (map (apply subst) rhs))
+
+theVarsIn :: Term -> [ VariableName ]
+theVarsIn (Var v@(VariableName _ _)) = [ v ]
+theVarsIn (Struct _ ts) = concatMap theVarsIn ts
+theVarsIn _  = []
+
+anySubsetOf :: [a] -> [[a]]
+anySubsetOf = map catMaybes . mapM (\v -> return Nothing <|> return (Just v))
